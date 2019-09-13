92dfe68182556748482a6b42a74ad2577547d561
mgreil <mgreil@mgreil-dump.42.fr>
2019-09-13 09:33:47 +0200

    add email to user db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    
+    def create
+        @user = User.new(user_params)
+
+        if @article.save
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
+
+    def signin
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :password)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_155156) do
+ActiveRecord::Schema.define(version: 2019_09_13_073225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_12_155156) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
     t.string "name"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
